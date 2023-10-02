781c96c900f249a54e379b835b2c444d564ee941
LorenzoPixel <71122234+HealthyFryOption@users.noreply.github.com>
2023-10-02 11:12:44 +0800

    Updated Model

--- Main/Computer_Vision.py
+++ Main/Computer_Vision.py
@@ -10,20 +10,19 @@
     see [ https://pytorch.org/tutorials/beginner/saving_loading_models.html ] for more info
 '''
 
+import importlib
+import GestureClassifier.COV_NET5 as NND
+import torch
+import numpy as np
+from cvzone.HandTrackingModule import HandDetector
+from time import perf_counter
+import mediapipe as mp
+import cv2 as cv
 import os
 from sys import path as sysPath
 
 os.chdir(sysPath[0])
 
-import cv2 as cv
-import mediapipe as mp
-from time import perf_counter
-from cvzone.HandTrackingModule import HandDetector
-
-import numpy as np
-import torch
-import GestureClassifier.COV_NET5 as NND
-import importlib
 
 # Constants
 mp_facedetector = mp.solutions.face_detection
@@ -40,34 +39,36 @@ IMG_HEIGHT = NND.IMG_HEIGHT
 
 model = NND.Neural()
 
-model.load_state_dict(torch.load("./GestureClassifier/model12_D_GHT_5(8,10).pth", map_location=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')))
+model.load_state_dict(torch.load("./GestureClassifier/ModelToUse.pth",
+                      map_location=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')))
 model.eval()
 
 LABELS = NND.CLASS_LABELS
 
 # HAND GESTURE MODEL
 
+
 class DrawComputer:
 
-    PADDING = 15 # padding to get hand gesture image in parseGesture()
+    PADDING = 15  # padding to get hand gesture image in parseGesture()
 
     def __init__(self):
         # Constant width and height in pixels of cv window
         self.width = 500
         self.height = 500
-    
+
     def parseGesture(self, bBox, frame):
 
         startX = max(bBox[0]-self.PADDING-10, 0)
         endX = max(bBox[0]+bBox[2]+self.PADDING, 0)
 
-        startY = max(bBox[1]-self.PADDING, 0) 
+        startY = max(bBox[1]-self.PADDING, 0)
         endY = max(bBox[1]+bBox[3]+self.PADDING, 0)
-        
+
         handImg = frame[startY:endY, startX:endX]
-        
+
         return handImg, [startX, startY, endX, endY]
-    
+
     # convert to GRAYSCALED Histogram Equalized
     def his_equ(self, read_img):
         # convert from BGR color-space to YCrCb
@@ -85,56 +86,62 @@ class DrawComputer:
     # Take frame image and manipulate it if needed
     def update_frame(self, frame, droneState, droneBattery):
         dictValue = {}
-        
+
         # START_TIMER
         start = perf_counter()
-        
+
         # Resize frame [need to take frame output]
         frame = cv.resize(frame, (self.width, self.height))
-        
-        
+
         # Any manipulation needed is done here based on state of drone
         if droneState == "facetrack":
             frame, dictValue["bBox"] = self.face_detect(frame)
-            
+
         elif droneState == "handtrack":
             frame, dictValue["bBox"] = self.hand_detect(frame)
-        
+
         elif droneState == "gesture":
             dictValue["bBox"] = self.hand_detect_for_gesture(frame)
-            dictValue["label"] = "" # initialise as empty string
-            
+            dictValue["label"] = ""  # initialise as empty string
+
             # if any hands were detected at all
             if dictValue["bBox"]:
                 handImg, newBbox = self.parseGesture(dictValue["bBox"], frame)
-            
+
                 # replace empty string with the name of the label detected from model
                 # Frame, HandImg, newBbox
-                dictValue["label"] = self.gesture_detect(frame, handImg, newBbox)
-        
+                dictValue["label"] = self.gesture_detect(
+                    frame, handImg, newBbox)
+
         # ========= FPS counter and outputting the image =========
         end = perf_counter()
         totalTime = end - start
-        
+
         if totalTime:
             fps = 1 / totalTime
-            cv.putText(frame, f'FPS: {int(fps)}', (20,30), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 2)
-        
-        cv.putText(frame, f'Battery: {int(droneBattery)}', (20,60), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 2)
-    
+            cv.putText(frame, f'FPS: {int(fps)}', (20, 30),
+                       cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+
+        cv.putText(frame, f'Battery: {int(droneBattery)}',
+                   (20, 60), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+
         if droneState == "keycontrols":
-            cv.putText(frame, f'Mode: Keyboard', (20,90), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 2)
+            cv.putText(frame, f'Mode: Keyboard', (20, 90),
+                       cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
         elif droneState == "facetrack":
-            cv.putText(frame, f'Mode: Face Tracking', (20,90), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 2)
+            cv.putText(frame, f'Mode: Face Tracking', (20, 90),
+                       cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
         elif droneState == "handtrack":
-            cv.putText(frame, f'Mode: Hand Tracking', (20,90), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 2)
+            cv.putText(frame, f'Mode: Hand Tracking', (20, 90),
+                       cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
         elif droneState == "gesture":
-            cv.putText(frame, f'Mode: Gesture Tracking', (20,90), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 2)
-        
+            cv.putText(frame, f'Mode: Gesture Tracking', (20, 90),
+                       cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+
         cv.imshow("Drone Camera", frame)
-        
+
         return dictValue
-    
+
     def hand_detect(self, frame):
         bBox = []
         hands, frame = detector.findHands(frame)
@@ -143,9 +150,8 @@ class DrawComputer:
             hand = hands[0]
             bBox = list(hand["bbox"])
             bBox.append(hand["center"])
-            
-        return frame, bBox
 
+        return frame, bBox
 
     def hand_detect_for_gesture(self, frame):
         bBox = []
@@ -154,17 +160,16 @@ class DrawComputer:
         if hands:
             hand = hands[0]
             bBox = list(hand["bbox"])
-    
-        return bBox
 
+        return bBox
 
     def gesture_detect(self, frame, handImg, newBbox):
 
         # Turn it GRAYSCALE as model accepts 1 channel input only
-        #handImg = cv.cvtColor(handImg, cv.COLOR_BGR2GRAY)
+        # handImg = cv.cvtColor(handImg, cv.COLOR_BGR2GRAY)
 
         handImg = self.his_equ(handImg)
-        
+
         # Resize it to what has been standardized
         handImg = cv.resize(handImg, (IMG_HEIGHT, IMG_WIDTH))
 
@@ -185,31 +190,32 @@ class DrawComputer:
         label = "UNKNOWN"
 
         # If probability of detection is more than the probability here
-        if(maxNum > 0.5):
+        if (maxNum > 0.5):
             answer = torch.argmax(prediction).item()
 
         if answer >= 0:
-                label = LABELS[answer]
-        
+            label = LABELS[answer]
+
         # Display current probability
         per = maxNum*100
 
-        cv.putText(frame, f"{label}: {per:.2f}%", (20, 120), cv.FONT_HERSHEY_SIMPLEX, 0.8, (255,0,0), 2)
-        cv.rectangle(frame, (newBbox[0], newBbox[1]), (newBbox[2], newBbox[3]), (255,0,0), 2)
+        cv.putText(frame, f"{label}: {per:.2f}%", (20, 120),
+                   cv.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
+        cv.rectangle(frame, (newBbox[0], newBbox[1]),
+                     (newBbox[2], newBbox[3]), (255, 0, 0), 2)
 
         return label
-        
 
     def face_detect(self, frame):
         with mp_facedetector.FaceDetection(model_selection=0, min_detection_confidence=0.60) as face_detection:
             bBox = []
-            
+
             # Convert BGR frame to RGB
             frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
 
             # Process the frame and find faces
             results = face_detection.process(frame)
-            
+
             # Convert the frame color back so it can be displayed
             frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
 
@@ -222,8 +228,10 @@ class DrawComputer:
 
                 h, w, c = frame.shape
 
-                boundBox = int(bBox.xmin * w), int(bBox.ymin * h), int(bBox.width * w), int(bBox.height * h)
+                boundBox = int(bBox.xmin * w), int(bBox.ymin *
+                                                   h), int(bBox.width * w), int(bBox.height * h)
 
-                cv.putText(frame, f'{int(detection.score[0]*100)}%', (boundBox[0], boundBox[1] - 20), cv.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 2)
-        
+                cv.putText(frame, f'{int(detection.score[0]*100)}%', (boundBox[0],
+                           boundBox[1] - 20), cv.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 2)
+
             return frame, bBox
